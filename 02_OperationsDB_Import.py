db5bf5ca5e8267aaf36839ee21a079d3ccebdd9b
Jeff Reichert <torres9fan@gmail.com>
2025-06-11 10:49:33 -0600

    Fix bracket and update README

--- 02_OperationsDB_Import.py
+++ 02_OperationsDB_Import.py
@@ -43,11 +43,11 @@ def main():
     try:
         # Gather List of DocumentIDs to migrate, using the ParentTypes from ParentLinks, see "gather_documentids.sql"
         logger.info("Gathering list of DocumentIDs that are in Scope for Supervision.")        
-        gather_documents_sql = load_sql('gather_documentids.sql')
+        gather_documents_sql = load_sql('operations/gather_documentids.sql')
         run_sql_script(target_conn, 'gather_documentids', gather_documents_sql)
 
         logger.info("Gathering list of Operations tables with SQL Commands to be migrated.")
-        additional_sql = load_sql('gather_drops_and_selects_Operations.sql')
+        additional_sql = load_sql('operations/gather_drops_and_selects_operations.sql')
         run_sql_script(target_conn, 'gather_drops_and_selects_Operations', additional_sql)
 
         # Import list of JOINS necessary to migrate each table in the Operations database.
@@ -74,7 +74,7 @@ def main():
             dtype={'Select_Into': Text(), 'Drop_IfExists': Text()}  
         )
         #Now we need to update the List of tables with the appropriate joins that we gathered from the csv file above.
-        update_joins_Operations_sql = load_sql('update_joins_Operations.sql')  
+        update_joins_Operations_sql = load_sql('operations/update_joins_operations.sql')
         run_sql_script(target_conn, 'update_joins_Operations', update_joins_Operations_sql)
         logger.info("Joins for Operations tables are updated")
         logger.info("Updating JOINS for Operations database is complete.")
@@ -121,7 +121,7 @@ def main():
         cursor.close()
         logger.info("All Drop_IfExists and Select_Into statements executed for Operations Database.")
         # Now that we are done with out SELECT INTO statements, we need to generate a list of Primary Keys and NOT Nullable Columns for each table.
-        pk_sql = load_sql('create_primarykeys_Operations.sql')  
+        pk_sql = load_sql('operations/create_primarykeys_operations.sql')
         run_sql_script(target_conn, 'create_primarykeys_Operations', pk_sql)
         logger.info("Generating List of Primary Keys and NOT Nullable Columns IN THE Operations DATABASE")
        # Now that we have our list, run through a cursor to execute each NOT NULL and Primary Key statement (in schema,table name order)
